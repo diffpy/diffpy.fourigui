aeb7e804bf3398a31dc5f097910d49cc56a365bc
Sam Andrello <samqandrello@gmail.com>
2024-08-28 12:46:43 -0400

    pytest unit_test.py

--- src/diffpy/fourigui/tests/unit_test.py
+++ src/diffpy/fourigui/tests/unit_test.py
@@ -1,150 +1,145 @@
-import unittest
-
 import h5py
+import pytest
 
 from diffpy.fourigui.fourigui import Gui
 
 
-class TestGui(unittest.TestCase):
-    def setUp(self):
-        # set up gui
-        self.test_gui = Gui()
-
-        # set up dummy data
-        self.dummydata = h5py.File("src/diffpy/fourigui/tests/testdata/dummydata.h5")["data"]
-
-    def test_init(self):
-        self.assertFalse(self.test_gui.loaded)
-        self.assertFalse(self.test_gui.transformed)
-        self.assertFalse(self.test_gui.cutted)
-        self.assertFalse(self.test_gui.transcutted)
-        self.assertFalse(self.test_gui.cutoff.get())
-        self.assertFalse(self.test_gui.space.get())
-
-    def test_load_cube_nothing_loaded(self):
-        # given
-        self.test_gui.filename_entry.delete(0, "end")
-        self.test_gui.filename_entry.insert(0, "src/diffpy/fourigui/tests/testdata/dummydata.h5")
-
-        # when
-        self.test_gui.load_cube()
-
-        # then
-        self.assertTrue(self.test_gui.loaded)
-
-    def test_load_cube_something_loaded(self):
-        # given
-        self.test_gui.loaded
-        self.test_gui.filename_entry.delete(0, "end")
-        self.test_gui.filename_entry.insert(0, "src/diffpy/fourigui/tests/testdata/dummydata.h5")
-
-        # when
-        self.test_gui.load_cube()
-
-        # then
-        self.assertTrue(self.test_gui.loaded)
-
-    def test_fft_000(self):
-        # given
-        self.test_gui.cube = self.dummydata
-        self.test_gui.plot_plane = (
-            lambda *a, **b: ()
-        )  # overwrite plot_plane which requires not initialized attribute im
-        self.test_gui.transformed = False
-        self.test_gui.transcutted = False
-        self.test_gui.cutoff.set(0)
-
-        # when
-        self.test_gui.fft()
-
-        # then
-        self.assertTrue(self.test_gui.transformed and not self.test_gui.transcutted)
-
-    def test_fft_010(self):
-        # given
-        self.test_gui.cube = self.dummydata
-        self.test_gui.plot_plane = (
-            lambda *a, **b: ()
-        )  # overwrite plot_plane which requires not initialized attribute im
-        self.test_gui.transformed = False
-        self.test_gui.transcutted = False
-        self.test_gui.cutoff.set(1)
-
-        # when
-        self.test_gui.fft()
-
-        # then
-        self.assertTrue(not self.test_gui.transformed and self.test_gui.transcutted)
-        # self.assertTrue(self.test_gui.cutted)
-
-    def test_fft_001(self):
-        # given
-        self.test_gui.cube = self.dummydata
-        self.test_gui.cube_reci = self.dummydata
-        self.test_gui.plot_plane = (
-            lambda *a, **b: ()
-        )  # overwrite plot_plane which requires not initialized attribute im
-        self.test_gui.transformed = False
-        self.test_gui.transcutted = True
-        self.test_gui.cutoff.set(0)
-
-        # when
-        self.test_gui.fft()
-
-        # then
-        self.assertTrue(self.test_gui.transformed and self.test_gui.transcutted)
-
-    def test_fft_011(self):
-        # given
-        self.test_gui.cube = self.dummydata
-        self.test_gui.cube_realcut = self.dummydata
-        self.test_gui.plot_plane = (
-            lambda *a, **b: ()
-        )  # overwrite plot_plane which requires not initialized attribute im
-        self.test_gui.transformed = False
-        self.test_gui.transcutted = True
-        self.test_gui.cutoff.set(1)
-
-        # when
-        self.test_gui.fft()
-
-        # then
-        self.assertTrue(not self.test_gui.transformed and self.test_gui.transcutted)
-
-    def test_fft_101(self):
-        # given
-        self.test_gui.cube = self.dummydata
-        self.test_gui.cube_real = self.dummydata
-        self.test_gui.plot_plane = (
-            lambda *a, **b: ()
-        )  # overwrite plot_plane which requires not initialized attribute im
-        self.test_gui.transformed = True
-        self.test_gui.transcutted = True
-        self.test_gui.cutoff.set(0)
-
-        # when
-        self.test_gui.fft()
-
-        # then
-        self.assertTrue(self.test_gui.transformed and self.test_gui.transcutted)
-
-    def test_fft_111(self):
-        # given
-        self.test_gui.cube = self.dummydata
-        self.test_gui.cube_realcut = self.dummydata
-        self.test_gui.plot_plane = (
-            lambda *a, **b: ()
-        )  # overwrite plot_plane which requires not initialized attribute im
-        self.test_gui.transformed = True
-        self.test_gui.transcutted = True
-        self.test_gui.cutoff.set(1)
-
-        # when
-        self.test_gui.fft()
-
-        # then
-        self.assertTrue(self.test_gui.transformed and self.test_gui.transcutted)
+@pytest.fixture
+def gui():
+    # set up gui
+    return Gui()
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def dummydata():
+    # set up dummy data
+    return h5py.File("src/diffpy/fourigui/tests/testdata/dummydata.h5")["data"]
+
+
+def test_init(gui):
+    assert not gui.loaded
+    assert not gui.transformed
+    assert not gui.cutted
+    assert not gui.transcutted
+    assert not gui.cutoff.get()
+    assert not gui.space.get()
+
+
+def test_load_cube_nothing_loaded(gui):
+    # given
+    gui.filename_entry.delete(0, "end")
+    gui.filename_entry.insert(0, "src/diffpy/fourigui/tests/testdata/dummydata.h5")
+
+    # when
+    gui.load_cube()
+
+    # then
+    assert gui.loaded
+
+
+def test_load_cube_something_loaded(gui):
+    # given
+    gui.loaded
+    gui.filename_entry.delete(0, "end")
+    gui.filename_entry.insert(0, "src/diffpy/fourigui/tests/testdata/dummydata.h5")
+
+    # when
+    gui.load_cube()
+
+    # then
+    assert gui.loaded
+
+
+def test_fft_000(gui):
+    # given
+    gui.cube = dummydata
+    gui.plot_plane = lambda *a, **b: ()  # overwrite plot_plane which requires not initialized attribute im
+    gui.transformed = False
+    gui.transcutted = False
+    gui.cutoff.set(0)
+
+    # when
+    gui.fft()
+
+    # then
+    assert gui.transformed and not gui.transcutted
+
+
+def test_fft_010(gui):
+    # given
+    gui.cube = dummydata
+    gui.plot_plane = lambda *a, **b: ()  # overwrite plot_plane which requires not initialized attribute im
+    gui.transformed = False
+    gui.transcutted = False
+    gui.cutoff.set(1)
+
+    # when
+    gui.fft()
+
+    # then
+    assert not gui.transformed and gui.transcutted
+    # assert gui.cutted)
+
+
+def test_fft_001(gui):
+    # given
+    gui.cube = dummydata
+    gui.cube_reci = dummydata
+    gui.plot_plane = lambda *a, **b: ()  # overwrite plot_plane which requires not initialized attribute im
+    gui.transformed = False
+    gui.transcutted = True
+    gui.cutoff.set(0)
+
+    # when
+    gui.fft()
+
+    # then
+    assert gui.transformed and gui.transcutted
+
+
+def test_fft_011(gui):
+    # given
+    gui.cube = dummydata
+    gui.cube_realcut = dummydata
+    gui.plot_plane = lambda *a, **b: ()  # overwrite plot_plane which requires not initialized attribute im
+    gui.transformed = False
+    gui.transcutted = True
+    gui.cutoff.set(1)
+
+    # when
+    gui.fft()
+
+    # then
+    assert not gui.transformed and gui.transcutted
+
+
+def test_fft_101(gui):
+    # given
+    gui.cube = dummydata
+    gui.cube_real = dummydata
+    gui.plot_plane = lambda *a, **b: ()  # overwrite plot_plane which requires not initialized attribute im
+    gui.transformed = True
+    gui.transcutted = True
+    gui.cutoff.set(0)
+
+    # when
+    gui.fft()
+
+    # then
+    assert gui.transformed and gui.transcutted
+
+
+def test_fft_111(gui):
+    # given
+    gui.cube = dummydata
+    gui.cube_realcut = dummydata
+    gui.plot_plane = lambda *a, **b: ()  # overwrite plot_plane which requires not initialized attribute im
+    gui.transformed = True
+    gui.transcutted = True
+    gui.cutoff.set(1)
+
+    # when
+    gui.fft()
+
+    # then
+    assert gui.transformed and gui.transcutted
