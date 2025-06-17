080fc8f9cc494e87fe0540dfe4e7601a07c191a1
tinatn29 <tanaporn.nanarong@gmail.com>
2025-06-17 16:05:17 -0400

    skpkg: mirate src folder

--- src/diffpy/fourigui/__init__.py
+++ src/diffpy/fourigui/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2022-2025 The Trustees of Columbia University in the City of New York.
+# (c) 2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Billinge Group members and community contributors.
+# File coded by: Billinge Group members.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/diffpy/diffpy.fourigui/graphs/contributors
@@ -15,7 +15,7 @@
 """Tool for visualizing 3D diffraction and PDF images."""
 
 # package version
-from diffpy.fourigui.version import __version__
+from diffpy.fourigui.version import __version__  # noqa
 
 # silence the pyflakes syntax checker
 assert __version__ or True
